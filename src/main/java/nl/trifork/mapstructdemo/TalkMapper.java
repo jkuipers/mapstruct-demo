8424a20ba4e9111f0629551022846b2d39a6259b
Joris Kuipers <joriskuipers@gmail.com>
2021-10-28 17:02:07 +0200

    Unmapped target property - ignore

--- src/main/java/nl/trifork/mapstructdemo/TalkMapper.java
+++ src/main/java/nl/trifork/mapstructdemo/TalkMapper.java
@@ -1,10 +1,12 @@
 package nl.trifork.mapstructdemo;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface TalkMapper {
 
+    @Mapping(target = "room", ignore = true)
     TalkDTO toDTO(Talk talk);
 
 }
