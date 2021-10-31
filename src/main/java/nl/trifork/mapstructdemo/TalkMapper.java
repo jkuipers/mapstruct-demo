912c4f3cfd19028dd5c1ac60943d56620456d21e
Joris Kuipers <joriskuipers@gmail.com>
2021-10-31 12:54:44 +0100

    URI to String via default method in TalkMapper

--- src/main/java/nl/trifork/mapstructdemo/TalkMapper.java
+++ src/main/java/nl/trifork/mapstructdemo/TalkMapper.java
@@ -5,11 +5,17 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ValueMapping;
 
+import java.net.URI;
+
 @Mapper
 public interface TalkMapper {
 
     @Mapping(target = "room", source = "location.name")
     TalkDTO toDTO(Talk talk);
+
+    default String fromURI(URI uri) {
+        return uri == null ? null : uri.toString();
+    }
 
     @ValueMapping(target = "OTHER", source = MappingConstants.ANY_REMAINING)
     TalkDTO.Type toDTO(Talk.Type type);
