b4327cea1c23e348a106411b717791f4bd2d417e
Joris Kuipers <joriskuipers@gmail.com>
2021-10-31 13:05:41 +0100

    Map Duration using @AfterMapping instead

--- src/main/java/nl/trifork/mapstructdemo/TalkMapper.java
+++ src/main/java/nl/trifork/mapstructdemo/TalkMapper.java
@@ -1,17 +1,26 @@
 package nl.trifork.mapstructdemo;
 
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ValueMapping;
+
+import java.time.Duration;
 
 @Mapper(uses = SharedMapper.class)
 public interface TalkMapper {
 
     @Mapping(target = "room", source = "location.name")
-    @Mapping(target = "durationInMinutes",
-             expression = "java(talk.getDuration() == null ? null : talk.getDuration().toMinutes())")
+    @Mapping(target = "durationInMinutes", ignore = true)
     TalkDTO toDTO(Talk talk);
+
+    @AfterMapping
+    default void duration(Talk talk, @MappingTarget TalkDTO dto) {
+        Duration duration = talk.getDuration();
+        if (duration != null) dto.setDurationInMinutes(duration.toMinutes());
+    }
 
     @ValueMapping(target = "OTHER", source = MappingConstants.ANY_REMAINING)
     TalkDTO.Type toDTO(Talk.Type type);
